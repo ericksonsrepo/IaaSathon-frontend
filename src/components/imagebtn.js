d22df108cce57bb62e23d1348c79d0099e2db013
ericksonsrepo <38170783+ericksonsrepo@users.noreply.github.com>
2018-09-17 10:20:57 -0700

    Update imagebtn.js

--- src/components/imagebtn.js
+++ src/components/imagebtn.js
@@ -9,35 +9,39 @@ class ImageBTN extends Component{
 
     }
 
-    determineCatorDog(props){
-        if (props.name === "dog"){
-            return "./images/dog.png"
-        } else if (props.name === "cat"){
-            return "./images/cat.svg"
+    determineFramework(props){
+        if (props.name === "angular"){
+            return "./images/angular.png"
+        } else if (props.name === "ember"){
+            return "./images/ember.png"
+        } else if (props.name === "react"){
+            return "./images/react.png"
+        } else if (props.name === "vue"){
+            return "./images/vue.png"
         }
     }
-    
+
     sendVote(e) {
         let url = "http://" + window.location.hostname + ":8000/submitPoll"
         let data = {
             "name": this.props.name
         }
-    
+
         axios.post(url, data)
             .then((resp)=>{
                 console.log(resp.data);
-                
+
             })
             .catch((err)=>{
                 console.log(err)
             });
-        
+
     }
 
-    
+
     render(){
-        return(                         
-            <img onClick={this.sendVote} src={this.determineCatorDog(this.props)} alt="cat or dog"/>
+        return(
+            <img onClick={this.sendVote} src={this.determineFramework(this.props)} alt="angular, ember, react, or vue"/>
         )
 
     }
